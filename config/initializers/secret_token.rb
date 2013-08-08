0fd37e8751823d84e8e98a0890e21c6f694c69a6
William Herry <WilliamHerryChina@Gmail.com>
2013-08-08 09:35:03 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liulishuo::Application.config.secret_key_base = 'f6995d6b5c3de985223167a184ebcf77dda5e117af67fe6a30c53e9bff5bfdde4152398ef4e89eed77b3de1139e26ccb287a025e5feacba9bdbfb01825a9e762'
