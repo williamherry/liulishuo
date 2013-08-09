618bca2726b996ced5ffddd4e6e58dca192df0fd
William Herry <WilliamHerryChina@Gmail.com>
2013-08-09 19:22:47 +0800

    fix default value for last_login_time

--- db/migrate/20130808025146_add_last_login_time_to_users.rb
+++ db/migrate/20130808025146_add_last_login_time_to_users.rb
@@ -1,5 +1,5 @@
 class AddLastLoginTimeToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :last_login_time, :datetime, default: 0
+    add_column :users, :last_login_time, :datetime
   end
 end
